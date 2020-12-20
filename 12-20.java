7760640eeca3682df3dfc9e8b74cef13c095d333
Ye (Eason) Sun <60046805+sunwenye09@users.noreply.github.com>
2020-12-20 15:33:15 -0800

    Update 12-20.java
    102. Binary Tree Level Order Traversal;
    107. Binary Tree Level Order Traversal II;
    103. Binary Tree Zigzag Level Order Traversal

--- 12-20.java
+++ 12-20.java
@@ -40,3 +40,73 @@ class Solution {
         return result;
     }
 }
+
+//107. Binary Tree Level Order Traversal II
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Stack<List<Integer>> stack = new Stack<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (queue.size() != 0) {
+            int n = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            stack.push(temp);
+        }
+        while (stack.size() != 0) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+}
+
+//103. Binary Tree Zigzag Level Order Traversal
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (queue.size() != 0) {
+            List<Integer> temp = new ArrayList<>();
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            List<Integer> subList = new ArrayList<>();
+            if (level % 2 == 0) {
+                subList = temp;
+            } else {
+                for (int i = temp.size() - 1; i >= 0; i--) {
+                    subList.add(temp.get(i));
+                }
+            }
+            result.add(subList);
+            level++;
+        }
+        return result;
+    }
+}
